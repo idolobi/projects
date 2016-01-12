f4ef426f9b741d0e76633c505398907e83bc382c
idolobi <euiseok.kang@gmail.com>
2016-01-12 15:13:19 +0900

    Create BaseRequestDto.java

---
+++ dms-exejar/src/com/es/deployer/deploy/dto/BaseRequestDto.java
@@ -0,0 +1,176 @@
+package com.es.deployer.deploy.dto;
+
+import java.sql.Timestamp;
+
+public class BaseRequestDto {
+	private String projId;
+	private String srcGrupCd; 
+	private String depReqId;
+	private String srId;
+	private String depReqDate;
+	private String rglrUrgntDivCd;
+	private String imprvErrDivCd;
+	private String devprId;
+	private String mpmNm;
+	private String mpmId;
+	private String wrkrNm;
+	private String wrkrId;
+	private String subject;
+	private String reqCnts;
+	private String depId;
+	private String note;
+	private String testBginDate;
+	private String testEndDate;
+	private String svcOpenPlanDate;
+	private String qryExamId;
+	private String firstInptUsrId;
+	private Timestamp firstInptTime;
+	private String lastModiUsrId;
+	private Timestamp lastModiTime;
+	
+	
+	public String getProjId() {
+		return projId;
+	}
+	public void setProjId(String projId) {
+		this.projId = projId;
+	}
+	public String getSrcGrupCd() {
+		return srcGrupCd;
+	}
+	public void setSrcGrupCd(String srcGrupCd) {
+		this.srcGrupCd = srcGrupCd;
+	}
+	public String getDepReqId() {
+		return depReqId;
+	}
+	public void setDepReqId(String depReqId) {
+		this.depReqId = depReqId;
+	}
+	public String getSrId() {
+		return srId;
+	}
+	public void setSrId(String srId) {
+		this.srId = srId;
+	}
+	public String getDepReqDate() {
+		return depReqDate;
+	}
+	public void setDepReqDate(String depReqDate) {
+		this.depReqDate = depReqDate;
+	}
+	public String getRglrUrgntDivCd() {
+		return rglrUrgntDivCd;
+	}
+	public void setRglrUrgntDivCd(String rglrUrgntDivCd) {
+		this.rglrUrgntDivCd = rglrUrgntDivCd;
+	}
+	public String getImprvErrDivCd() {
+		return imprvErrDivCd;
+	}
+	public void setImprvErrDivCd(String imprvErrDivCd) {
+		this.imprvErrDivCd = imprvErrDivCd;
+	}
+	public String getDevprId() {
+		return devprId;
+	}
+	public void setDevprId(String devprId) {
+		this.devprId = devprId;
+	}
+	public String getMpmNm() {
+		return mpmNm;
+	}
+	public void setMpmNm(String mpmNm) {
+		this.mpmNm = mpmNm;
+	}
+	public String getMpmId() {
+		return mpmId;
+	}
+	public void setMpmId(String mpmId) {
+		this.mpmId = mpmId;
+	}
+	public String getWrkrNm() {
+		return wrkrNm;
+	}
+	public void setWrkrNm(String wrkrNm) {
+		this.wrkrNm = wrkrNm;
+	}
+	public String getWrkrId() {
+		return wrkrId;
+	}
+	public void setWrkrId(String wrkrId) {
+		this.wrkrId = wrkrId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getReqCnts() {
+		return reqCnts;
+	}
+	public void setReqCnts(String reqCnts) {
+		this.reqCnts = reqCnts;
+	}
+	public String getDepId() {
+		return depId;
+	}
+	public void setDepId(String depId) {
+		this.depId = depId;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getTestBginDate() {
+		return testBginDate;
+	}
+	public void setTestBginDate(String testBginDate) {
+		this.testBginDate = testBginDate;
+	}
+	public String getTestEndDate() {
+		return testEndDate;
+	}
+	public void setTestEndDate(String testEndDate) {
+		this.testEndDate = testEndDate;
+	}
+	public String getSvcOpenPlanDate() {
+		return svcOpenPlanDate;
+	}
+	public void setSvcOpenPlanDate(String svcOpenPlanDate) {
+		this.svcOpenPlanDate = svcOpenPlanDate;
+	}
+	public String getQryExamId() {
+		return qryExamId;
+	}
+	public void setQryExamId(String qryExamId) {
+		this.qryExamId = qryExamId;
+	}
+	public String getFirstInptUsrId() {
+		return firstInptUsrId;
+	}
+	public void setFirstInptUsrId(String firstInptUsrId) {
+		this.firstInptUsrId = firstInptUsrId;
+	}
+	public Timestamp getFirstInptTime() {
+		return firstInptTime;
+	}
+	public void setFirstInptTime(Timestamp firstInptTime) {
+		this.firstInptTime = firstInptTime;
+	}
+	public String getLastModiUsrId() {
+		return lastModiUsrId;
+	}
+	public void setLastModiUsrId(String lastModiUsrId) {
+		this.lastModiUsrId = lastModiUsrId;
+	}
+	public Timestamp getLastModiTime() {
+		return lastModiTime;
+	}
+	public void setLastModiTime(Timestamp lastModiTime) {
+		this.lastModiTime = lastModiTime;
+	}
+}
